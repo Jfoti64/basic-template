8467e20d5b1050771f3e9875efc0ef9c4383fb47
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-10 20:25:05 -0500

    Add babel to webpack and add .babelrc.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
